86add6dca45d4d85f501440f53d797872429fb74
vlasta_stelmakh <vlasta.stelmakh2.0@gmail.com>
2021-10-12 17:08:53 +0300

    fix: fix upload
    check if element exists in collection and if not then upload image name to the gallery

--- public/appLogic/upload.js
+++ public/appLogic/upload.js
@@ -1,33 +1,112 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.uploadImg = void 0;
 var fs = require("fs");
 var path = require("path");
+var server_1 = require("../server");
 var ImageSchema_1 = require("../database/models/ImageSchema");
 function uploadImg(req, res) {
-    if (req.files.photo.size != '0') {
-        console.log(req.files.photo.path);
-        console.log(path.join(path.resolve("../static/photos"), req.files.photo.name));
-        fs.rename(req.files.photo.path, path.join(path.resolve("../static/photos"), req.files.photo.name), function (err) {
-            if (err)
-                throw err;
-        });
-        var img_1 = req.files.photo.name;
-        fs.stat(path.join(__dirname, "../../static/photos/" + img_1), function (err, data) {
-            if (err) {
-                console.log(err);
+    return __awaiter(this, void 0, void 0, function () {
+        var isReal, img_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (!(req.files.photo.size != '0')) return [3 /*break*/, 2];
+                    console.log(req.files.photo.name);
+                    return [4 /*yield*/, isExist(req.files.photo.name)];
+                case 1:
+                    isReal = _a.sent();
+                    console.log('IS REAL: ', isReal);
+                    if (isReal) {
+                        console.log('done');
+                        return [2 /*return*/];
+                    }
+                    else {
+                        console.log("CONTINUED");
+                        console.log(req.files.photo.path);
+                        console.log(path.join(path.resolve("../static/photos"), req.files.photo.name));
+                        fs.rename(req.files.photo.path, path.join(path.resolve("../static/photos"), req.files.photo.name), function (err) {
+                            if (err)
+                                throw err;
+                        });
+                        img_1 = req.files.photo.name;
+                        fs.stat(path.join(__dirname, "../../static/photos/" + img_1), function (err, data) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                var image = new ImageSchema_1.default({
+                                    path: img_1,
+                                    metadata: data
+                                });
+                                image.save().then(function (result) { return console.log(result); });
+                            }
+                        });
+                    }
+                    return [3 /*break*/, 3];
+                case 2:
+                    fs.unlink(req.files.photo.path, function () { });
+                    _a.label = 3;
+                case 3: return [2 /*return*/];
             }
-            else {
-                var image = new ImageSchema_1.default({
-                    path: img_1,
-                    metadata: data
-                });
-                image.save().then(function (result) { return console.log(result); });
-            }
         });
-    }
-    else {
-        fs.unlink(req.files.photo.path, function () { });
-    }
+    });
 }
 exports.uploadImg = uploadImg;
+function isExist(imagePath) {
+    return __awaiter(this, void 0, void 0, function () {
+        var collection, isReal;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, server_1.db.collection('users')];
+                case 1:
+                    collection = _a.sent();
+                    return [4 /*yield*/, collection.find({ path: { $exists: imagePath } }, function (err, result) {
+                            if (result) {
+                                return true;
+                            }
+                        })];
+                case 2:
+                    isReal = _a.sent();
+                    console.log("is Real func: ", isReal);
+                    return [2 /*return*/, isReal];
+            }
+        });
+    });
+}
--- src/appLogic/upload.ts
+++ src/appLogic/upload.ts
@@ -6,28 +6,54 @@ import { db } from '../server'
 import ImageModel from '../database/models/ImageSchema'
 
 
-export function uploadImg(req: any, res: any){
+export async function uploadImg(req: any, res: any){
     if (req.files.photo.size != '0') {
-        console.log(req.files.photo.path)
-        console.log(path.join(path.resolve("../static/photos"), req.files.photo.name))
-        fs.rename(req.files.photo.path, path.join(path.resolve("../static/photos"), req.files.photo.name), (err) => {
-            if (err) throw err
-         });
-        let img = req.files.photo.name
-        fs.stat(path.join(__dirname, `../../static/photos/${img}`), (err: any, data: any) => {
-                if(err){
-                    console.log(err)
-                } else {
-                    const image = new ImageModel({
-                        path: img,
-                        metadata: data
-                    })
+        console.log(req.files.photo.name)
+        // isExist(req.files.photo.name)
+        let isReal: any = await isExist(req.files.photo.name)
+        console.log('IS REAL: ', isReal)
+        if(isReal){
+            console.log('done')
+            return
+        } else {
 
-                    image.save().then((result: any) => console.log(result))
-                }
-            })
+            console.log("CONTINUED")
+    
+            console.log(req.files.photo.path)
+            console.log(path.join(path.resolve("../static/photos"), req.files.photo.name))
+            fs.rename(req.files.photo.path, path.join(path.resolve("../static/photos"), req.files.photo.name), (err) => {
+                if (err) throw err
+             });
+            let img = req.files.photo.name
+            fs.stat(path.join(__dirname, `../../static/photos/${img}`), (err: any, data: any) => {
+                    if(err){
+                        console.log(err)
+                    } else {
+                        const image = new ImageModel({
+                            path: img,
+                            metadata: data
+                        })
+    
+                        image.save().then((result: any) => console.log(result))
+                    }
+                })
+        }
     } else {
         fs.unlink(req.files.photo.path, () => { });
     }
+}
+
+async function isExist(imagePath: string){
+    let collection = await db.collection('users')
+
+    let isReal = await collection.find({path : { $exists: imagePath}}, function(err: any, result: any){
+        if(result){
+
+            return true
+        }
+    })
+    console.log("is Real func: ",isReal)
+
+    return isReal
 }
 
